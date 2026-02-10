1f0135c3619cfac002a8e78974f254e9c197cd0a
NicatDev <nicat254memmedov@gmail.com>
2026-02-10 17:58:26 +0400

    main

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -67,7 +67,7 @@ CHANNEL_LAYERS = {
         "default": {
             "BACKEND": "channels_redis.core.RedisChannelLayer",
             "CONFIG": {
-                "hosts": [("localhost", 6379)],
+                "hosts": [("localhost", 6380)],
         },
     },
 }
