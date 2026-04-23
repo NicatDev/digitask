8c7bcb7564e944110943960305c72cb8005f1fdb
NicatDev <nicat254memmedov@gmail.com>
2026-04-23 22:59:59 +0400

    main

--- backend/audit/urls.py
+++ backend/audit/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import AuditLogViewSet
 
 router = DefaultRouter()
-router.register(r'logs', AuditLogViewSet)
+router.register(r'logs', AuditLogViewSet, basename='auditlog')
 
 urlpatterns = [
     path('', include(router.urls)),
